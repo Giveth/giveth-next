69ffb7395d648e7ce16353f42790b505a45d6959
Mateo Daza <mateodaza@gmail.com>
2021-11-16 07:18:49 -0500

    Update myAccount.js

--- src/components/account/myAccount.js
+++ src/components/account/myAccount.js
@@ -7,7 +7,7 @@ import { Context as Web3Context } from '../../contextProvider/Web3Provider'
 
 const MyAccount = ({ info }) => {
   const {
-    state: { user, account }
+    state: { user, account },
   } = useContext(Web3Context)
 
   const [openModal, setOpenModal] = useState(false)
@@ -24,22 +24,30 @@ const MyAccount = ({ info }) => {
   //   }
   //   init()
   // }, [])
-  console.log({ userssss: user })
   return (
     <>
-      <EditProfileModal isOpen={openModal} onRequestClose={() => setOpenModal(false)} />
+      <EditProfileModal
+        isOpen={openModal}
+        onRequestClose={() => setOpenModal(false)}
+      />
 
       {!user?.name && (
         <Notification
-          content='Please finish setting up your public profile.'
+          content="Please finish setting up your public profile."
           action={{ title: 'Complete Profile', do: () => setOpenModal(true) }}
-          type='warn'
+          type="warn"
         />
       )}
 
       <Flex>
-        <Avatar img={user?.profileImage || user?.avatar} size={100} address={account} />
-        <Flex sx={{ flexDirection: 'column', ml: '27px', textAlign: 'flex-end' }}>
+        <Avatar
+          img={user?.profileImage || user?.avatar}
+          size={100}
+          address={account}
+        />
+        <Flex
+          sx={{ flexDirection: 'column', ml: '27px', textAlign: 'flex-end' }}
+        >
           <Text sx={{ color: 'secondary', fontSize: 7 }}>{user?.name}</Text>
           <Text sx={{ color: 'bodyDark', fontSize: 3 }}>{user?.email}</Text>
           <Text
@@ -48,7 +56,7 @@ const MyAccount = ({ info }) => {
               color: 'primary',
               fontSize: 3,
               variant: 'links.default',
-              mt: 2
+              mt: 2,
             }}
           >
             Edit Public Profile
@@ -56,7 +64,9 @@ const MyAccount = ({ info }) => {
         </Flex>
       </Flex>
       <Flex sx={{ mt: '40px', alignItems: 'center' }}>
-        <Text sx={{ textTransform: 'uppercase', fontSize: 0 }}>Wallet Address</Text>
+        <Text sx={{ textTransform: 'uppercase', fontSize: 0 }}>
+          Wallet Address
+        </Text>
         {/* <Button
           type='button'
           sx={{
@@ -69,7 +79,9 @@ const MyAccount = ({ info }) => {
           Change
         </Button> */}
       </Flex>
-      <Text sx={{ mt: '14px', variant: 'text.medium', color: 'secondary' }}>{account}</Text>
+      <Text sx={{ mt: '14px', variant: 'text.medium', color: 'secondary' }}>
+        {account}
+      </Text>
       <Flex sx={{ mt: '40px' }}>
         <Flex
           sx={{
@@ -79,19 +91,21 @@ const MyAccount = ({ info }) => {
             paddingTop: '20px',
             paddingLeft: '24px',
             backgroundColor: '#F4F6FC',
-            borderRadius: '12px'
+            borderRadius: '12px',
           }}
         >
           <Text
             sx={{
               fontSize: 0,
               color: 'secondary',
-              textTransform: 'uppercase'
+              textTransform: 'uppercase',
             }}
           >
             My donations
           </Text>
-          <Text sx={{ color: 'primary', fontSize: 7 }}>{info?.myDonations}</Text>
+          <Text sx={{ color: 'primary', fontSize: 7 }}>
+            {info?.myDonations}
+          </Text>
         </Flex>
         <Flex
           sx={{
@@ -102,19 +116,21 @@ const MyAccount = ({ info }) => {
             paddingLeft: '24px',
             backgroundColor: '#F4F6FC',
             borderRadius: '12px',
-            ml: '5%'
+            ml: '5%',
           }}
         >
           <Text
             sx={{
               fontSize: 0,
               color: 'secondary',
-              textTransform: 'uppercase'
+              textTransform: 'uppercase',
             }}
           >
             My projects
           </Text>
-          <Text sx={{ color: 'primary', fontSize: 7 }}>{info?.myProjects || 0}</Text>
+          <Text sx={{ color: 'primary', fontSize: 7 }}>
+            {info?.myProjects || 0}
+          </Text>
         </Flex>
       </Flex>
       {/* <Box
