04a905de20640204c529e66aa6f407eada1cb351
Ramin <r.ramazanpour@aut.ac.ir>
2021-12-21 15:21:10 +0330

    remove unused packages

--- src/components/styled-components/Input.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Email_Input = styled.input`
-  width: 360px;
-  height: 52px;
-  border: 1px solid #d7ddea;
-  border-radius: 56px;
-  padding: 14px 25px;
-`
