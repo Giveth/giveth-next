1882e84b7d2397066413905d19e57d147d8bc99a
Mateo Daza <mateodaza@gmail.com>
2021-12-20 20:14:03 -0500

    Update transak.js

--- pages/api/transak.js
+++ pages/api/transak.js
@@ -1,5 +1,7 @@
 const GetTransakApiKey = (req, res) => {
-  res.status(200).json({ apiKey: process.env.TRANSAK_API_KEY })
+  // TODO: Blocking this as it's not safe
+  // res.status(200).json({ apiKey: process.env.TRANSAK_API_KEY })
+  res.status(200).json({ apiKey: null })
 }
 
 export default GetTransakApiKey
